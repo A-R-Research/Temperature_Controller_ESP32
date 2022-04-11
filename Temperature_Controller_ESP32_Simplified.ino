93ea63ea7e970cdf07af2eeefb50a04e50121c32
rex.smith <rex.smith2011@gmail.com>
2022-04-12 01:04:17 +0200

    ZN based tuning parameters

--- Temperature_Controller_ESP32_Simplified.ino
+++ Temperature_Controller_ESP32_Simplified.ino
@@ -2,7 +2,7 @@
 
 float Input = 0.0;
 float Output = 0.0;
-float Setpoint = 0.0;
+float Setpoint = 70;
 float rampTemp = 70;    //Temperature Setpoint in (degC)
 float rampTime = 60;    //Rate of chage of temperature
 float inputOld = 0.0, inputNew = 0.0;
@@ -11,20 +11,20 @@ unsigned long msBefore = 0, msBefore1 = 0 , seconds = 0;
 
 const int SetpointGap = 15;
 const int PIDCutOff = 5;
-float consKp = 0.5;
-float consKi = 0.1;
-float consKd = 0.9;
-float aggKp = 1.1;
+float consKp = 1.1;      //0.2*Ku, w/ Ku = 5
+float consKi = 0.075;    //0.4*Ku/Tu, w/ Tu = 80 sec
+float consKd = 17;
+float aggKp = 1.01;
 float aggKi = 0.95;
 float aggKd = 9;
-const unsigned long windowSize = 100;
+const unsigned long windowSize = 1000;
 const byte debounce = 0;
 unsigned long windowStartTime = 0, nextSwitchTime = 0;
 boolean relayStatus = false;
 QuickPID myPID(&Input, &Output, &Setpoint, consKp, consKi, consKd,
-               myPID.pMode::pOnErrorMeas,
+               myPID.pMode::pOnError,
                myPID.dMode::dOnMeas,
-               myPID.iAwMode::iAwClamp,
+               myPID.iAwMode::iAwCondition,
                myPID.Action::direct);
 
 Temp_Sensor tempProbe = Temp_Sensor(TemperatureProbeType);
@@ -37,7 +37,7 @@ void setup()
   sensorReadInterval = windowSize;
   //myPID.SetTunings(Kp, Ki, Kd);
   myPID.SetOutputLimits(0, windowSize);
-  myPID.SetSampleTimeUs(windowSize * 1000);
+  myPID.SetSampleTimeUs((windowSize/2) * 1000);
 
   myPID.SetMode(myPID.Control::automatic);
   delay(1000); //for stabality
@@ -46,12 +46,13 @@ void setup()
 void loop()
 {
   unsigned long msNow = millis();
-  if (msNow - msBefore >= sensorReadInterval)
-  {
-    inputNew = Input;
-    Input = tempProbe.readTemp();
-    msBefore = msNow;
-  }
+  Input = tempProbe.readTemp();
+//  if (msNow - msBefore >= sensorReadInterval)
+//  {
+//    inputNew = Input;
+//    Input = tempProbe.readTemp();
+//    msBefore = msNow;
+//  }
 
   Serial.print("SetPoint_Temp(degC):"); Serial.print(Setpoint); Serial.print(",");
   Serial.print("Actual_Temp(degC):"); Serial.print(Input); Serial.print(",");
