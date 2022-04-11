ae819670e4cbf3a760086906cb0c1ad638164ead
rex.smith <rex.smith2011@gmail.com>
2022-04-11 21:27:12 +0200

    Initial commit

--- Temp_Sensor.cpp
+++ Temp_Sensor.cpp
@@ -9,41 +9,10 @@
 Temp_Sensor::Temp_Sensor(bool sensorType)
 {
   _sensorType = sensorType;
-#ifdef MAX6675
-  pinMode(ThermocoupleCS, OUTPUT);
-  pinMode(ThermocoupleSCK, OUTPUT);
-  pinMode(ThermocoupleSO, INPUT);
-  digitalWrite(ThermocoupleCS, HIGH);
-  Serial.println("Thermocouple Pins set!");
-#else
-  //do nothing
-#endif
 }
 
 float Temp_Sensor::readTemp(void)
 {
-#ifdef MAX6675
-  if (millis() >= timeNow + sensorReadDelay_ms)
-  {
-    uint16_t spi = 0;
-    digitalWrite(ThermocoupleCS, LOW);
-    delayMicroseconds(10);
-    spi = getSPI();
-    spi <<= 8;
-    spi |= getSPI();
-    digitalWrite(ThermocoupleCS, HIGH);
-    if (spi & 0x4)  //Thermocouple not found!
-    {
-      return NAN;
-    }
-    spi >>= 3;
-    Tc = spi * 0.25;
-    T = Tc + 273.15;
-    Tf = Tc * 9 / 5 + 32;
-    timeNow += sensorReadDelay_ms;
-  }
-  return Tc;
-#else
     float Vout = 0.0;
     float Rt = 0.0;
     float adc = 0.0;
@@ -66,26 +35,4 @@ float Temp_Sensor::readTemp(void)
     Tc = T - 273.15;                   // Celsius
     Tf = Tc * 9 / 5 + 32;              // Fahrenheit
   return Tc;
-#endif
 }
-
-#ifdef MAX6675
-byte Temp_Sensor::getSPI(void)
-{
-  byte spiData = 0;
-  for (int i = 7; i >= 0; i--)
-  {
-    digitalWrite(ThermocoupleSCK, LOW);
-    delayMicroseconds(10);
-    if (digitalRead(ThermocoupleSO))
-    {
-      spiData |= (1 << i);
-    }
-    digitalWrite(ThermocoupleSCK, HIGH);
-    delayMicroseconds(10);
-  }
-  return spiData;
-}
-#else
-//do nothing
-#endif
--- Temperature_Controller_ESP32_Simplified.ino
+++ Temperature_Controller_ESP32_Simplified.ino
@@ -9,22 +9,6 @@ float inputOld = 0.0, inputNew = 0.0;
 unsigned long sensorReadInterval = 0.0;
 unsigned long msBefore = 0, msBefore1 = 0 , seconds = 0;
 
-//-------------------------------------Set these gains if DAC is selected---------------------------------------------
-#ifdef DACout
-float Kp = 0.35;
-float Ki = 0.6;
-float Kd = 3;
-QuickPID myPID(&Input, &Output, &Setpoint);
-
-//-------------------------------------Set these gains if PWM is selected--------------------------------------------
-#elif defined(PWMout)
-float Kp = 0.9;
-float Ki = 1.6;
-float Kd = 10;
-QuickPID myPID(&Input, &Output, &Setpoint);
-
-//-------------------------------------Set these gains if Digital is selected-------------------------------------------
-#else
 const int SetpointGap = 15;
 const int PIDCutOff = 5;
 float consKp = 0.5;
@@ -42,7 +26,6 @@ QuickPID myPID(&Input, &Output, &Setpoint, consKp, consKi, consKd,
                myPID.dMode::dOnMeas,
                myPID.iAwMode::iAwClamp,
                myPID.Action::direct);
-#endif
 
 Temp_Sensor tempProbe = Temp_Sensor(TemperatureProbeType);
 
@@ -53,14 +36,8 @@ void setup()
   Input = tempProbe.readTemp();
   sensorReadInterval = windowSize;
   //myPID.SetTunings(Kp, Ki, Kd);
-#ifdef DACout
-  myPID.SetOutputLimits(51, 53);    //from the DAC pin, value 51 is 0.66v (0 VAC from SSR), 52 is 0.67v (120 VAC from SSR), 53 is 0.69v (240 VAC from SSR)
-#elif defined(PWMout)
-  myPID.SetOutputLimits(141, 1023); //from PWM pin, value 141 is 0 VAC from SSR and 1023 is 240 VAC from SSR. The voltage is controlled mostly linearly in between (this needs to be tested!).
-#else
   myPID.SetOutputLimits(0, windowSize);
   myPID.SetSampleTimeUs(windowSize * 1000);
-#endif
 
   myPID.SetMode(myPID.Control::automatic);
   delay(1000); //for stabality
@@ -78,48 +55,7 @@ void loop()
 
   Serial.print("SetPoint_Temp(degC):"); Serial.print(Setpoint); Serial.print(",");
   Serial.print("Actual_Temp(degC):"); Serial.print(Input); Serial.print(",");
-
-#ifdef DACout
-  myPID.Compute();
-  dacWrite(SSR_Pin, Output);
-  Serial.print("ControllerOutput(V):");  Serial.print((float)((supplyVoltage  / 255)*Output));
-#elif defined(PWMout)
-  myPID.Compute();
-  ledcWrite(PWMChannel, Output);
-  Serial.print("ControllerOutput(V):");  Serial.print((float)((supplyVoltage / MAX_DUTY_CYCLE )*Output));
-
-
-#else
-#ifdef ConstantSetpoint
-  Setpoint = rampTemp;
-#else
-  if (Input < rampTemp || seconds <= rampTime)
-  {
-    Setpoint = seconds * (rampTemp / rampTime);                //Reach 90ºC till 60s (90/60=1.5)
-    if (Setpoint >= rampTemp)
-    {
-      Setpoint = rampTemp;
-    }
-  }
-#endif
-  float gap = abs(Setpoint - Input); //distance away from setpoint
-  if(gap <= PIDCutOff)
-  {
-    myPID.SetMode(myPID.Control::manual);
-  }
-  else
-  {
-    myPID.SetMode(myPID.Control::automatic);
-  }
-  if (abs(inputNew - inputOld) >= 0.3 || gap < SetpointGap)
-  { //we're close to setpoint, use conservative tuning parameters
-    myPID.SetTunings(consKp, consKi, consKd);
-  }
-  else
-  {
-    //we're far from setpoint, use aggressive tuning parameters
-    myPID.SetTunings(aggKp, aggKi, aggKd);
-  }
+  
   if (myPID.Compute())
   {
     windowStartTime = msNow;
@@ -146,7 +82,6 @@ void loop()
   Serial.print("Kp:"); Serial.print(myPID.GetKp()); Serial.print(",");
   Serial.print("Ki:"); Serial.print(myPID.GetKi()); Serial.print(",");
   Serial.print("Kd:"); Serial.print(myPID.GetKd());
-#endif
 
   Serial.println();
   if (msNow - msBefore1 >= 1000)
@@ -159,13 +94,6 @@ void loop()
 
 void initOutput(void)
 {
-#ifdef DACout
-  dacWrite(SSR_Pin, 0);
-#elif defined(PWMout)
-  ledcSetup(PWMChannel, PWMFreq, PWMResolution);
-  ledcAttachPin(SSR_Pin, PWMChannel);
-  ledcWrite(PWMChannel, 0);
-#else
+
   pinMode(SSR_Pin, OUTPUT);
-#endif
 }
